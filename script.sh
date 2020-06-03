f911c082f77e805ff60ce1d2f28366e800fbf7b6
SaiyanVegeta <yadul.raghav.cse15@itbhu.ac.in>
2020-06-03 19:51:51 +0530

    script_run

--- script.sh
+++ script.sh
@@ -4,25 +4,24 @@
 #SBATCH --output=job.%J.out
 #SBATCH --partition=gpu
 #SBATCH --nodelist=gpu[005]
-#SBATCH --output=test.out
 #SBATCH --job-name="roles"
 
 hostname
 # module load conda-python/3.7
-module load cuda/9.0
+module load cuda/10.1
 module load cuDNN/cuda_9.2_7.2.1
 
-# conda activate env1
-# python predict.py karate
-# python predict.py dolphins
-# python predict.py jazz
-# python predict.py usair97
-python predict.py fb_U
-python predict.py GrQc_U
-python predict.py PB_U
-python predict.py power
-python predict.py hep-th
-python predict.py cora
-python predict.py celegansneural
-python predict.py netscience
-python predict.py yeast
+conda activate roles
+python run.py r karate
+python run.py r dolphins
+python run.py r jazz
+python run.py r usair97
+python run.py r fb_U
+python run.py r GrQc_U
+python run.py r PB_U
+python run.py r power
+python run.py r adjnoun
+python run.py r celegansneural
+python run.py r netscience
+python run.py r LesMiserables
+python run.py r football
