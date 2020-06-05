2b94e69fa23a67b2113ff725f48929db1b8fb104
SaiyanVegeta <yadul.raghav.cse15@itbhu.ac.in>
2020-06-05 12:15:59 +0530

    ok

--- script.sh
+++ script.sh
@@ -16,11 +16,12 @@ module load cuDNN/cuda_9.2_7.2.1
 # python run.py r dolphins
 # python run.py r jazz
 # python run.py r usair97
-python run.py r fb_U
+# python run.py r fb_U
+python run.py a GrQc_U
 python run.py r GrQc_U
-python run.py r PB_U
+# python run.py r PB_U
 # python run.py r adjnoun
 # python run.py r celegansneural
-python run.py r netscience
+# python run.py r netscience
 # python run.py r LesMiserables
 # python run.py r football
